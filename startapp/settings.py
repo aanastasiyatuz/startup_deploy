6222f34849514da2e0ccf5ddb5875a0da6e7c5d6
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-05-24 23:17:03 +0600

    .

--- startapp/settings.py
+++ startapp/settings.py
@@ -129,15 +129,15 @@ USE_TZ = True
 
 FIRST_DAY_OF_WEEK = 1
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'travel-startup/static'),
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
