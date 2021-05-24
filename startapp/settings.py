d427d5329f817524b7191d8a60b9610b974e35ee
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-05-24 21:40:44 +0600

    added static

--- startapp/settings.py
+++ startapp/settings.py
@@ -130,10 +130,15 @@ USE_TZ = True
 FIRST_DAY_OF_WEEK = 1
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'account.MyUser'
