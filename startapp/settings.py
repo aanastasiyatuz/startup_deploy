e65ea97692992b30afa1513245202551a73f8453
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-05-24 21:50:58 +0600

    added locallibrary to requirements.txt

--- startapp/settings.py
+++ startapp/settings.py
@@ -138,7 +138,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'account.MyUser'
